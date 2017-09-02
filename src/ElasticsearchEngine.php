8b27910319bd114be53041c9f14d25a39401e26f
Bertel Torp <bertel@ulobby.dk>
2017-09-02 20:29:29 +0200

    Remove delete&update

--- src/ElasticsearchEngine.php
+++ src/ElasticsearchEngine.php
@@ -37,24 +37,7 @@ class ElasticsearchEngine extends Engine
      */
     public function update($models)
     {
-        $params['body'] = [];
 
-        $models->each(function($model) use (&$params)
-        {
-            $params['body'][] = [
-                'update' => [
-                    '_id' => $model->getKey(),
-                    '_index' => $this->index,
-                    '_type' => $model->searchableAs(),
-                ]
-            ];
-            $params['body'][] = [
-                'doc' => $model->toSearchableArray(),
-                'doc_as_upsert' => true
-            ];
-        });
-
-        $this->elastic->bulk($params);
     }
 
     /**
@@ -65,20 +48,7 @@ class ElasticsearchEngine extends Engine
      */
     public function delete($models)
     {
-        $params['body'] = [];
 
-        $models->each(function($model) use (&$params)
-        {
-            $params['body'][] = [
-                'delete' => [
-                    '_id' => $model->getKey(),
-                    '_index' => $this->index,
-                    '_type' => $model->searchableAs(),
-                ]
-            ];
-        });
-
-        $this->elastic->bulk($params);
     }
 
     /**
